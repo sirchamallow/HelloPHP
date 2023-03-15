f0561162772415444f8359000b8c2c7dcd6cd358
Pierre LEMASSON <sir@chamallow.com>
2023-03-15 11:16:24 +0100

    Hello World en PHP

---
+++ helloworld.php
@@ -0,0 +1,13 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+      <title>Hello PHP !</title>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  </head>
+  <body>
+    <p>
+      <?php
+        echo "Hello world !";
+      ?>
+    </p>
+  </body>
+</html>
